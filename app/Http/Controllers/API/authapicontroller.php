8c6bbfff7700d7d99df1c57c5647b8a1649f2988
Lovilek <wingeddemon2274@gmail.com>
2022-11-22 00:47:34 +0600

    Initial commit

---
+++ app/Http/Controllers/API/authapicontroller.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use phpseclib3\Crypt\Hash;
+
+class authapicontroller extends Controller
+{
+    public function register(Request $request){
+       $user =User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>\Illuminate\Support\Facades\Hash::make($request->password),
+
+        ]);
+       return response()->json([
+           'success'=>true,
+           'token'=>$user->createToken($user->email)->accessToken
+       ]);
+    }
+    public function login(Request $request){
+        $credentials=$request->only('email','password');
+        $users=User::where('email',$credentials['email'])->first();
+        if(!$users){
+            return response()->json([
+               'success'=>false,
+                'message'=>'Invalid email'
+            ],401);
+
+        }
+
+        if(Auth::attempt($credentials)){
+            return response()->json([
+                'success'=>true,
+                'token'=>$users->createToken($users->email)->accessToken
+            ],200);
+        }
+        return response()->json([
+            'success'=>false,
+            'message'=>'Invalid email or password'
+        ],401);
+    }
+}
