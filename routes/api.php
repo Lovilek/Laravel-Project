2c78b6f9bcd94bec15d82a169397e5a1a6fb62cd
Lovilek <wingeddemon2274@gmail.com>
2022-11-22 00:47:17 +0600

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,14 @@ Route::post('/fottballcreateapi',[\App\Http\Controllers\API\FootballerController
 Route::get('/fottballapi/{id}',[\App\Http\Controllers\API\FootballerControllerApi::class,'findId']);
 Route::post('/fottballupdateapi/{id}',[\App\Http\Controllers\API\FootballerControllerApi::class,'update']);
 Route::post('/fottballdeleteapi/{id}',[\App\Http\Controllers\API\FootballerControllerApi::class,'delete']);
+Route::post('register',[\App\Http\Controllers\api\authapicontroller::class,'register']);
+Route::post('login',[\App\Http\Controllers\api\authapicontroller::class,'login']);
+
+Route::group(['middleware'=>'auth:api'],function (){
+    Route::get('emps',[\App\Http\Controllers\api\employeecontroller::class,'index']);
+
+});
+
 
 
 
